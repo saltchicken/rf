0b4121c70f828cce105467ed4a22f444ff2e8183
saltchicken <john.eicher89@gmail.com>
2025-05-19 19:09:26 -0700

    Record uses num_samples

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ def main():
     
     try:
         # Record from first FM station (92.1 MHz)
-        samples = recorder.record(center_freq=92.0e6, duration_seconds=4, freq_offset=3e5)
+        samples = recorder.record_duration(center_freq=92.0e6, duration_seconds=3, freq_offset=3e5)
         recorder.fm_demodulate_and_save(samples, "station1.wav")
         
         # Record from second FM station (95.5 MHz)
-        samples = recorder.record(center_freq=95.5e6, duration_seconds=4, freq_offset=3e5)
+        samples = recorder.record_duration(center_freq=95.5e6, duration_seconds=3, freq_offset=3e5)
         recorder.fm_demodulate_and_save(samples, "station2.wav")
         
     finally:
