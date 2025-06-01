89fff38c25ca99d728ecc06f5dd91298782def5a
saltchicken <john.eicher89@gmail.com>
2025-06-01 13:39:52 -0700

    Fix project run

--- src/rf/__main__.py
+++ src/rf/__main__.py
@@ -1,10 +1,17 @@
 import uvicorn
 import os
 
+from pathlib import Path
+
+frontend_dir = f"{Path(__file__).parent}/frontend"
+
+current_dir = Path(__file__).parent
+print(f"Current directory: {current_dir}")
+
 
 def main():
-    os.chdir("frontend")
-    uvicorn.run("frontend.main:app", host="127.0.0.1", port=5000)
+    os.chdir(frontend_dir)
+    uvicorn.run("rf.frontend.main:app", host="127.0.0.1", port=5000)
 
 
 if __name__ == "__main__":
