0e8c73bc5ba8f344dba6cd0ecdde759ea1415379
saltchicken <john.eicher89@gmail.com>
2025-06-01 13:54:05 -0700

    Run build from main cli

--- src/rf/__main__.py
+++ src/rf/__main__.py
@@ -1,17 +1,33 @@
 import uvicorn
 import os
-
+import argparse
 from pathlib import Path
 
 frontend_dir = f"{Path(__file__).parent}/frontend"
-
 current_dir = Path(__file__).parent
 print(f"Current directory: {current_dir}")
 
 
 def main():
-    os.chdir(frontend_dir)
-    uvicorn.run("rf.frontend.main:app", host="127.0.0.1", port=5000)
+    parser = argparse.ArgumentParser(description="Run or build the frontend.")
+    parser.add_argument(
+        "command",
+        nargs="?",
+        default="run",
+        help="Command to run (e.g., 'build' or 'run')",
+    )
+
+    args = parser.parse_args()
+
+    if args.command == "build":
+        os.chdir(frontend_dir)
+        os.system("bun install")
+        os.system("bun run build")
+
+        # For example: os.system("npm run build")
+    else:
+        os.chdir(frontend_dir)
+        uvicorn.run("rf.frontend.main:app", host="127.0.0.1", port=5000)
 
 
 if __name__ == "__main__":
